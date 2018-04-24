582e1ab3737e457dc1d7bef89e6865b4ba9ff7cc
Bruce <sanchez.uragan@gmail.com>
2018-04-24 17:51:23 +0300

    create memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,16 @@
-export function memoize() {
-    // TODO
+export function memoize(fn) {
+    const cache = new Map();
+    const wrapper = function dec(...args) {
+        const hash = (args.length === 0) ? 0 : JSON.stringify(args);
+        if (cache.has(hash)) {
+            return cache.get(hash);
+        }
+        cache.set(hash, fn.apply(this, args));
+        return cache.get(hash);
+    };
+    if (!(typeof fn === 'function')) {
+        return null;
+    }
+    return wrapper;
 }
+
